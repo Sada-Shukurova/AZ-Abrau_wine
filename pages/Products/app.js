37f3f3c5f1fa9c90800c2967adca37a77ec64a17
sada shukurova <shukurova.sada@gmail.com>
2023-08-15 14:11:37 +0400

    add products page

---
+++ pages/Products/app.js
@@ -0,0 +1,20 @@
+const productsContainer = document.querySelector('.products-container')
+
+fetch("http://localhost:3000/wines").then((res) => res.json())
+    .then(data => {
+        data.forEach((card) => {
+            const { title, description, image, spirt } = card;
+            productsContainer.innerHTML += `
+        <div class="products-card d-flex flex-column align-center">
+        <a class="products-card-link" href=""></a>
+            <img src="${image}" alt="">
+            <h3 class="products-card-title">${title}</h3>
+            <p class="products-card-description">Çeşid: ${description}</p>
+            <p class="spirt">Spirt: ${spirt}</p>
+
+            <a class="products-card-btn" href="">Ətraflı</a>
+        </div>
+        `
+        })
+    }).catch((error) => console.log(error))
+
