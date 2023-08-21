62ba0192f0625bf9ebc73ec66ab5cb27ab364bd1
sada shukurova <shukurova.sada@gmail.com>
2023-08-21 16:22:35 +0400

    add about swiper and other sections

--- pages/About/about.js
+++ pages/About/about.js
@@ -1,3 +1,45 @@
+// fancybox video
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-  });
+  //custom options
+});
+
+// swiper for awards
+let swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 1000,
+  },
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 6,
+  spaceBetween: 10,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    900:{
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    },
+    1180:{
+      slidesPerView: 6,
+      spaceBetween: 10
+    },
+
+}});
