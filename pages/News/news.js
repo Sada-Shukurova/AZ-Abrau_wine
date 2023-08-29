2595b522c74771c9490af5b339600c15b6879ce5
sada shukurova <shukurova.sada@gmail.com>
2023-08-29 18:59:33 +0400

    js added

--- pages/News/news.js
+++ pages/News/news.js
@@ -32,29 +32,48 @@ mobileCloseIcon.addEventListener('click', function () {
 
 
 // fetching data
-document.addEventListener("DOMContentLoaded", getTopNews);
-document.addEventListener("DOMContentLoaded", getAllNews);
+
 
 
 const url = "https://azabrau-backend.vercel.app/news";
 const newsTop = document.querySelector(".news-top");
 const newsBottom = document.querySelector(".news-bottom");
 
-function getTopNews() {
+document.addEventListener("DOMContentLoaded", getData)
+
+
+// function for fetching data
+function getData() {
     fetch(url)
-        .then(res => res.json()
-            .then(data => {
-                newsTop.innerHTML = "";
-                const topData = data.slice(0, 1)
-                topData.forEach(item => {
-                    const { newsTitle,
-                        newsDescription,
-                        image,
-                        date
-                    } = item;
+        .then(resp => resp.json())
+        .then(data => {
+            // console.log(data);
+
+            getTopNews(data[0]);
+
+            const slicedData = data.slice(1, data.length);
+            getAllNews(slicedData)
+        }).catch(err => { console.log(err); });
+}
 
 
-                    newsTop.innerHTML += `
+// functions for writing data
+
+function getTopNews(topData) {
+
+
+    // newsTop.innerHTML = "";
+    // const topData = data.slice(0, 1)
+    const { id,
+        newsTitle,
+        newsDescription,
+        image,
+        date
+    } = topData
+
+
+
+    newsTop.innerHTML += `
                 <div class="news-top-left">
                     <img src="${image}" alt="wine bottle">
                 </div>
@@ -62,34 +81,36 @@ function getTopNews() {
                     <h2 class="news-top-heading">${newsTitle}</h2>
                     <h3 class="news-top-date">${date}</h3>
                     <p class="news-top-description">${newsDescription}</p>
-                    <a href="../singleNews/singleNews.html?id=${id}" class="news-top-btn">Ətraflı</a>
+                    <a href="../SingleNews/singleNews.html?id=${id}" class="news-top-btn">Ətraflı</a>
                 </div>
                     `;
-                })
-            })).catch((error) => console.log(error))
+
+
 };
 
-function getAllNews() {
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
+function getAllNews(allNews) {
+    console.log(allNews)
 
-            newsBottom.innerHTML = "";
-            const slicedData = data.slice(1, 7)
-            slicedData.forEach((news) => {
-                const { newsTitle,
-                    image,
-                    date
-                } = news;
+    newsBottom.innerHTML = "";
+    // const slicedData = data.slice(1, data.length)
+    // console.log(slicedData)
+    allNews.forEach((news) => {
 
-                newsBottom.innerHTML += `
+        const {
+            id, newsTitle,
+            image,
+            date
+        } = news;
+
+
+
+        newsBottom.innerHTML += `
                 <div class="news-bottom-container">
-                    <a href="#newsB" class="newsLink-bottom"></a>
+                    <a href="../SingleNews/singleNews.html?id=${id}" class="newsLink-bottom"></a>
                     <img src="${image}" alt="wine bottles">
                     <h2 class="news-bottom-heading">${newsTitle}</h2>
                     <h3 class="news-bottom-date">${date}</h3>
                 </div>
                 `
-            })
-        }).catch((error) => console.log(error))
+    })
 }
--- pages/SingleNews/singleNews.js
+++ pages/SingleNews/singleNews.js
@@ -35,11 +35,13 @@ mobileCloseIcon.addEventListener('click', function () {
 document.addEventListener("DOMContentLoaded", getSingleNews);
 
 const singleCardContainer = document.querySelector(".single-card-container");
+const currentNews = document.querySelector('.currentNews')
 
 function getSingleNews() {
     const newsID = new URL(window.location.href).searchParams.get('id');
     const newsURL = `https://azabrau-backend.vercel.app/news/${newsID}`;
-    
+
+
     fetch(newsURL)
         .then((res) => res.json())
         .then((data) => {
@@ -58,6 +60,7 @@ function getSingleNews() {
                 <p class="single-card-date">${date}</p>
                 <h3 class="single-card-description">${newsDescription}</h3>
             `;
+            currentNews.textContent = `${newsTitle}`
         })
         .catch((error) => console.log(error));
 }
